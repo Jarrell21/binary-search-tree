cdd34a7a97030d3c251b83387006500c3c864704
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-12-14 00:19:27 +0800

    Add insert and delete functions

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,55 +1,88 @@
-const Node = (data) => {
+const Node = (value) => {
     const left = null;
     const right = null;
 
-    return { data, left, right }
+    return { value, left, right }
 }
 
 const Tree = (array) => {
-    let root = null;
+    const sortedArr = [...new Set(array)].sort((a,b) => a-b);
+    let root = buildTree(sortedArr, 0, sortedArr.length-1);
 
-    const buildTree = (array, start, end) => {
+    function buildTree(sortedArr, start, end){
         if(start > end) return null;
 
-        array.sort((a, b)=>{
-            if(a > b) return 1;
-            if(a < b) return -1;
-            return 0;
-        })
-
         const mid = parseInt((start+end) / 2);
-        const node = Node(array[mid]);
+        const node = Node(sortedArr[mid]);
 
-        node.left = buildTree(array, start, mid - 1);
-        node.right = buildTree(array, mid + 1, end);
+        node.left = buildTree(sortedArr, start, mid - 1);
+        node.right = buildTree(sortedArr, mid + 1, end);
 
         return node;
     }
 
     const insert = (value) => {
-        
+        root = insertRec(root, value);
     }
 
-    return { buildTree };
+    const insertRec = (root, value) => {
+        if(root === null){
+            root = Node(value);
+            return root;
+        }
+
+        if(value < root.value) root.left = insertRec(root.left, value);
+        else if(value > root.value) root.right = insertRec(root.right, value);
+
+        return root;
+    }
+
+    const deleteNode = (value) =>  {
+        root = deleteRec(root, value);
+    };
+
+    const deleteRec = (root, value) => {
+        if(root === null) return root;
+
+        if(value < root.value) root.left = deleteRec(root.left, value);
+        else if (value > root.value) root.right = deleteRec(root.right, value);
+        else{
+            if(root.left === null) return root.right;
+            else if(root.right === null) return root.left;
+
+            root.value = minValue(root.right);
+            root.right = deleteRec(root.right, root.value);
+        }
+
+        return root;
+    }
+
+    function minValue(root){
+        let minv = root.value;
+        while(root.left != null){
+            minv = root.left.value;
+            root.left;
+        }
+        return minv;
+    }
+
+    return { insert, deleteNode, root };
 }
+let sampleArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const myTree = Tree(sampleArray);
+myTree.insert(11);
+myTree.deleteNode(11);
 
 // log tree in a structured format
 const prettyPrint = (node, prefix = '', isLeft = true) => {
     if (node.right !== null) {
         prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
     }
-    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
     if (node.left !== null) {
         prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
 }
 
-let sampleArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-
-// remove duplicates
-sampleArray = [...new Set(sampleArray)];
-
-const myTree = Tree();
-const myBuiltTree = myTree.buildTree(sampleArray, 0, (sampleArray.length-1));
-
-prettyPrint(myBuiltTree);
+prettyPrint(myTree.root);
